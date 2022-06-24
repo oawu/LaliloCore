8b1e8e8e553cfc33ecc745d67eadd1077d867064
OA Wu <oa.wu@idrip.coffee>
2022-06-24 17:04:21 +0800

    Fix code.

--- Serve/index.js
+++ Serve/index.js
@@ -52,7 +52,7 @@ module.exports = {
         isDirectory(Config.entry) || fail(null, Path.relative(Setting.root, Config.entry) + Path.sep + ' 不是目錄類型')
 
         // php
-        Config.php = php || Typeof.object.or(Config.php, {})
+        Config.php = Typeof.object.or(php, Typeof.object.or(Config.php, {}))
         Config.php.enable = Typeof.bool.or(Config.php.enable, false)
         Config.php.maxBuffer = Typeof.num.or(Config.php.maxBuffer, 1024 * 1024)
         Config.php.env = Typeof.str.do.or(Config.php.env, env => ['Development', 'Testing', 'Staging', 'Production'].includes(env) ? env : null, 'Development')
